0aae04257bb2bfd5053e07b239874814e91f6b9b
Ousmane Samba <ousmanesamba@gmail.com>
2015-02-03 19:06:35 +0000

    login

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -10,30 +10,30 @@
 angular.module('childfundApp')
   .controller('UserCtrl', function ($scope,$location) {
       // Register the login() function
-      //$scope.login = function() {
-      //   $location.url('home');
-      //  $http.post('/login', {
-      //     email: $scope.user.email,
-      //     password: $scope.user.password
-      //  })
-      //    .success(function(response) {
-      //       // authentication OK
-      //       $scope.loginError = 0;
-      //       $rootScope.user = response.user;
-      //       $rootScope.$emit('loggedin');
-      //       if (response.redirect) {
-      //         if (window.location.href === response.redirect) {
-      //           //This is so an admin user will get full admin page
-      //           window.location.reload();
-      //         } else {
-      //           window.location = response.redirect;
-      //         }
-      //       } else {
-      //         $location.url('/');
-      //       }
-      //    })
-      //    .error(function() {
-      //       $scope.loginerror = 'Authentication failed.';
-      //    });
-      //};
+      $scope.login = function() {
+         $location.url('home');
+        //$http.post('/login', {
+        //   email: $scope.user.email,
+        //   password: $scope.user.password
+        //})
+        //  .success(function(response) {
+        //     // authentication OK
+        //     $scope.loginError = 0;
+        //     $rootScope.user = response.user;
+        //     $rootScope.$emit('loggedin');
+        //     if (response.redirect) {
+        //       if (window.location.href === response.redirect) {
+        //         //This is so an admin user will get full admin page
+        //         window.location.reload();
+        //       } else {
+        //         window.location = response.redirect;
+        //       }
+        //     } else {
+        //       $location.url('/');
+        //     }
+        //  })
+        //  .error(function() {
+        //     $scope.loginerror = 'Authentication failed.';
+        //  });
+      };
   });
